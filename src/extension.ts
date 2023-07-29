b133d18a20a8200481a80b56fa35c4773cd76524
Akash Sannasi <sannasi2@illinois.edu>
2023-07-29 17:58:43 -0500

    Attempt to highlight then edit text

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,48 @@
+"use strict";
+
+import { strict } from "assert";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log("Welcome to unix-to-utc!");
+  const disposable = vscode.commands.registerCommand(
+    "unix-to-utc.conversion",
+    () => {
+      const editor = vscode.window.activeTextEditor;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "unix-to-utc" is now active!');
+      if (editor) {
+        const document = editor.document;
+        const selection = editor.selection;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('unix-to-utc.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Unix to UTC!');
-	});
+        const text = document.getText(selection);
 
-	context.subscriptions.push(disposable);
+        console.log(text);
+        editor.edit((editBuilder) => {
+          editBuilder.replace(selection, "123213");
+        });
+      }
+    }
+  );
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable2 = vscode.commands.registerCommand(
+    "unix-to-utc.HelloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World from Unix to UTC!");
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
