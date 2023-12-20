11102976dfffa3fc89381562695d850041e40e50
Akash Sannasi <sannasi2@illinois.edu>
2023-12-20 10:31:58 -0800

    add test cases using pytest

--- src/datetime_api.py
+++ src/datetime_api.py
@@ -1,38 +1,44 @@
-from fastapi import FastAPI
-from datetime import datetime
+from fastapi import FastAPI, Response, status
+from datetime import datetime, timezone
 
 app = FastAPI()
 
 # maybe can cache later for efficiency
 
 
-def validate(date_text) -> bool:
-    try:
-        parser.parse(date_text)
-        return True
-    except ValueError:
-        return False
-
-
 # uvicorn datetime_api:app --reload
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to unix to utc world"}
 
 
-@app.get("/convert/{date}")
-async def convert(date):
+@app.get("/current_unix_timestamp")
+async def current_unix_timestamp():
+    current_utc_time = datetime.utcnow()
+    current_utc_timestamp = current_utc_time.replace(tzinfo=timezone.utc).timestamp()
+    return {"unix": current_utc_timestamp}
+
+
+@app.get("/current_timestamp")
+async def current_unix_timestamp():
+    current_utc_time = datetime.utcnow()
+    return {"unix": current_utc_time}
+
+
+@app.get("/convert/{date}", status_code=status.HTTP_200_OK)
+async def convert(date, response: Response):
+    date_time = None
     try:
-        # Check if the date is an integer (timestamp)
         if date.isdigit():
             date = int(date)
             date_time = datetime.utcfromtimestamp(date)
+            return {"date": date_time}
         else:
             # If it's not an integer, assume it's a string in the format '%Y-%m-%dT%H:%M:%S'
-            date_time = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S")
-            da
-        return {"date": date_time}, 200
+            date_object = datetime.fromisoformat(date)
+            utc_timestamp = date_object.replace(tzinfo=timezone.utc).timestamp()
+            return {"date": utc_timestamp}
 
     except ValueError as e:
-        # Handle parsing errors
-        return {"error": f"Invalid date format: {e}"}, 400
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {"date": "error"}
